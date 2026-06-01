9b748518909d8061efc18da4f6ebb1d1056a75e7
isayanx9 <isayanx9@gmail.com>
2026-06-01 16:29:31 +0530

    Switch to Cashfree payment links

--- database/payment.py
+++ database/payment.py
@@ -12,7 +12,7 @@ def create_cashfree_payment_link(
     customer_id
 ):
 
-    url = "https://api.cashfree.com/pg/orders"
+    url = "https://api.cashfree.com/pg/links"
 
     headers = {
         "accept": "application/json",
@@ -23,13 +23,14 @@ def create_cashfree_payment_link(
     }
 
     payload = {
-        "order_id": order_id,
-        "order_amount": amount,
-        "order_currency": "INR",
         "customer_details": {
-            "customer_id": str(customer_id),
+            "customer_name": f"User_{customer_id}",
             "customer_phone": "9999999999"
-        }
+        },
+        "link_amount": amount,
+        "link_currency": "INR",
+        "link_id": order_id,
+        "link_purpose": f"Coupon Purchase {order_id}"
     }
 
     response = requests.post(
@@ -40,6 +41,7 @@ def create_cashfree_payment_link(
 
     data = response.json()
 
+    print("PAYMENT LINK RESPONSE:")
     print(data)
 
     return data
